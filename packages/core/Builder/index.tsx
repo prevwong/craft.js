4a9a6f7b2ab544a2a3c7f0b427d06e44a183c1b1
Prev Wong <prevwong@gmail.com>
2019-06-05 03:43:55 +0800

    🍻 Added drag events on NodeElement

--- packages/core/Builder/index.tsx
+++ packages/core/Builder/index.tsx
@@ -1,24 +1,35 @@
 import React from "react";
 import NodeElement from "../Nodes/NodeElement";
-import { Nodes, CanvasNodes } from "~types";
+import { Nodes, CanvasNodes, NodeId } from "~types";
+import console = require("console");
 
 
 interface BuilderContext {
   nodes: Nodes,
   canvases: CanvasNodes,
+  active: Node,
+  dragging: Node,
   setCanvasNodes: Function
+  setActive: Function
+  setDragging: Function
 }
 
 export const BuilderContext = React.createContext<BuilderContext>({
   canvases: null,
   nodes: null,
-  setCanvasNodes: () => { }
+  active: null,
+  dragging: null,
+  setCanvasNodes: () => { },
+  setActive: () => { },
+  setDragging: () => { }
 });
 
 export default class Builder extends React.Component {
   state: BuilderContext = {
     nodes: {},
     canvases: {},
+    active: null,
+    dragging: null,
     setCanvasNodes: (canvasId: string, nodes: Node[], parentNodeId: string) => {
       if (!this.state.canvases[canvasId]) {
         this.state.canvases[canvasId] = [];
@@ -33,6 +44,16 @@ export default class Builder extends React.Component {
         ...this.state.nodes,
         ...nodes
       } as Nodes
+    },
+    setActive: (id: NodeId) => {
+      this.setState({
+        active: this.state.nodes[id]
+      });
+    },
+    setDragging: (id: NodeId) => {
+      this.setState({
+        dragging: id ? this.state.nodes[id] : null
+      });
     }
   }
   componentDidMount() {
