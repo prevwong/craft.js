b8dae700062e8fd84aa02a11703e1e11f9ee9956
Prev Wong <prevwong@gmail.com>
2019-06-12 23:20:55 +0800

    [feature/#1] 🚚 move findPosition and movePlaceholder to helper

--- packages/core/Builder/DragDropManager/helper.ts
+++ packages/core/Builder/DragDropManager/helper.ts
@@ -1,13 +1,70 @@
-import {
-  id,
-  DOMInfo,
-  DropAction
-} from "~types";
-import { NodeInfo, Node, Nodes, NodeId } from "~types/tree";
-import console = require("console");
+import { DropAction, DOMInfo, CanvasNode, NodeInfo, NodeId } from "~types";
 
-const findPosition = (
-  parent: Index,
+export const movePlaceholder = (
+  pos: DropAction,
+  canvasDOMInfo: DOMInfo, // which canvas is cursor at
+  bestTargetDomInfo: DOMInfo // closest element in canvas (null if canvas is empty)
+) => {
+  let marg = 0,
+    t = 0,
+    l = 0,
+    w = 0,
+    h = 0,
+    margin = {},
+    margI = 5,
+    brd = 3,
+    where = pos.where;
+
+  const elDim = bestTargetDomInfo ? bestTargetDomInfo : null;
+
+  margin = {
+    top: -brd,
+    left: 0,
+    bottom: 0,
+    right: 0
+  };
+
+  if (elDim) {
+    // If it's not in flow (like 'float' element)
+    if (!elDim.inFlow) {
+      w = null;
+      h = elDim.outerHeight - marg * 2;
+      t = elDim.top + marg;
+      l =
+        where == "before" ? elDim.left - marg : elDim.left + elDim.outerWidth - marg;
+      margin = {
+        top: 0,
+        left: -brd,
+        bottom: 0,
+        right: 0
+      };
+    } else {
+      w = elDim.outerWidth;
+      h = null;
+      t =
+        where == "before" ? elDim.top - marg : elDim.top + elDim.outerHeight - marg;
+      l = elDim.left;
+    }
+  } else {
+    if (canvasDOMInfo) {
+      t = canvasDOMInfo.top + margI;
+      l = canvasDOMInfo.left + margI;
+      w = canvasDOMInfo.outerWidth - margI * 2;
+      h = null;
+    }
+  }
+
+  return {
+    top: t,
+    left: l,
+    width: w,
+    height: h,
+    margin
+  };
+}
+
+export const findPosition = (
+  parent: CanvasNode,
   dims: NodeInfo[],
   posX: number,
   posY: number
@@ -30,16 +87,16 @@ const findPosition = (
   // Each dim is: Top, Left, Height, Width
   for (var i = 0, len = dims.length; i < len; i++) {
     dim = dims[i].dom;
-    id = dims[i].id as id;
+    id = dims[i].id as NodeId;
 
     // Right position of the element. Left + Width
-    dimRight = dim.left + dim.width;
+    dimRight = dim.left + dim.outerWidth;
     // Bottom position of the element. Top + Height
-    dimDown = dim.top + dim.height;
+    dimDown = dim.top + dim.outerHeight;
     // X center position of the element. Left + (Width / 2)
-    xCenter = dim.left + dim.width / 2;
+    xCenter = dim.left + dim.outerWidth / 2;
     // Y center position of the element. Top + (Height / 2)
-    yCenter = dim.top + dim.height / 2;
+    yCenter = dim.top + dim.outerHeight / 2;
     // Skip if over the limits
     if (
       (xLimit && dim.left > xLimit) ||
@@ -70,143 +127,4 @@ const findPosition = (
   }
 
   return result;
-};
-
-const movePlaceholder = (
-  pos: DropAction,
-  canvasDOMInfo: DOMInfo, // which canvas is cursor at
-  bestTargetDomInfo: DOMInfo // closest element in canvas (null if canvas is empty)
-) => {
-  let marg = 0,
-    t = 0,
-    l = 0,
-    w = 0,
-    h = 0,
-    margin = {},
-    margI = 5,
-    brd = 3,
-    where = pos.where;
-
-  const elDim = bestTargetDomInfo ? bestTargetDomInfo : null;
-
-  margin = {
-    top: -brd,
-    left: 0,
-    bottom: 0,
-    right: 0
-  };
-
-  if (elDim) {
-    // If it's not in flow (like 'float' element)
-    if (!elDim.inFlow) {
-      w = null;
-      h = elDim.height - marg * 2;
-      t = elDim.top + marg;
-      l =
-        where == "before" ? elDim.left - marg : elDim.left + elDim.width - marg;
-      margin = {
-        top: 0,
-        left: -brd,
-        bottom: 0,
-        right: 0
-      };
-    } else {
-      w = elDim.width;
-      h = null;
-      t =
-        where == "before" ? elDim.top - marg : elDim.top + elDim.height - marg;
-      l = elDim.left;
-    }
-  } else {
-    if (canvasDOMInfo) {
-      t = canvasDOMInfo.top + margI;
-      l = canvasDOMInfo.left + margI;
-      w = canvasDOMInfo.width - margI * 2;
-      h = null;
-    }
-  }
-
-  return {
-    top: t,
-    left: l,
-    width: w,
-    height: h,
-    margin
-  };
-};
-
-const getNearestTarget = (
-  e: MouseEvent,
-  nodes: Nodes,
-  dragNode: Node
-) => {
-  const pos = { x: e.clientX, y: e.clientY };
-  const { canvas } = dragNode;
-  const nodesWithinBounds = canvas ? (Object.keys(nodes).filter(id => {
-    // don't include children of current node; do not want to drop our current element into it's children
-    return (
-      !canvas.includes(id) && id !== "rootNode"
-    );
-  })) : Object.keys(nodes).filter(id => id !== "rootNode");
-
-  return nodesWithinBounds.filter((nodeId: NodeId) => {
-    const { info } = nodes[nodeId];
-    const { dom, id } = info;
-    const { top, left, width, height } = dom;
-    return (
-      (pos.x >= left && pos.x <= left + width) &&
-      (pos.y >= top && pos.y <= top + height)
-    );
-  });
-};
-
-export const placeBestPosition = (
-  nodes: Nodes,
-  dragTarget: Node,
-  e: MouseEvent
-) => {
-  const nearestTargets = getNearestTarget(e, nodes, dragTarget),
-    nearestTargetId = nearestTargets.pop();
-
-  if (nearestTargetId) {
-    const targetNode = nodes[nearestTargetId],
-      targetParent = targetNode.parent ? nodes[targetNode.parent] : null;
-
-    if (targetParent) {  // target is part of a canvas
-      console.log(targetParent)
-    } else { // target is a lone canvas
-
-    }
-
-    // const targetIndex = tree.getIndex(nearestTarget.id),
-    //   targetParent = targetIndex.children
-    //     ? targetIndex
-    //     : tree.getIndex(targetIndex.parent),
-    //   targetParentInfo = tree.getInfo(targetParent.id);
-
-    // const dimensionsInContainer = tree.getChildInfo(false, targetParent.id);
-
-    // const bestTarget = findPosition(
-    //   targetParent,
-    //   dimensionsInContainer,
-    //   e.clientX,
-    //   e.clientY
-    // );
-
-    // const bestTargetNode = tree.get(targetParent.children[bestTarget.index]);
-
-    // return {
-    //   position: movePlaceholder(
-    //     bestTarget,
-    //     targetParentInfo.dom,
-    //     targetParent.children.length
-    //       ? tree.getInfo(bestTargetNode.id).dom
-    //       : null
-    //   ),
-    //   node: bestTargetNode,
-    //   placement: bestTarget
-    // };
-  }
-
-  return null;
-};
+}
--- packages/core/Builder/DragDropManager/index.tsx
+++ packages/core/Builder/DragDropManager/index.tsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import { DropAction, BuilderContextState, Nodes, NodeId, CanvasNode, NodeInfo, DOMInfo } from "~types";
+import { DropAction, BuilderContextState, Nodes, NodeId, CanvasNode } from "~types";
 import BuilderContext from "../BuilderContext";
 import { isCanvas } from "~src/utils";
 import Canvas from "~packages/core/Canvas";
 import Placeholder from "./Placeholder";
+import { movePlaceholder, findPosition } from "./helper";
 
 class DragDropManager extends Component {
   lastPos: DropAction;
@@ -42,11 +43,11 @@ class DragDropManager extends Component {
         }
       })
 
-      const bestTarget = this.findPosition(targetParent, dimensionsInContainer, e.clientX, e.clientY);
+      const bestTarget = findPosition(targetParent, dimensionsInContainer, e.clientX, e.clientY);
       const bestTargetNode = nodes[targetParent.nodes[bestTarget.index]];
 
       const output = {
-        position: this.movePlaceholder(
+        position: movePlaceholder(
           bestTarget,
           targetParent.info.dom,
           targetParent.nodes.length
@@ -63,134 +64,7 @@ class DragDropManager extends Component {
 
 
 
-  movePlaceholder(
-    pos: DropAction,
-    canvasDOMInfo: DOMInfo, // which canvas is cursor at
-    bestTargetDomInfo: DOMInfo // closest element in canvas (null if canvas is empty)
-  ) {
-    let marg = 0,
-      t = 0,
-      l = 0,
-      w = 0,
-      h = 0,
-      margin = {},
-      margI = 5,
-      brd = 3,
-      where = pos.where;
 
-    const elDim = bestTargetDomInfo ? bestTargetDomInfo : null;
-
-    margin = {
-      top: -brd,
-      left: 0,
-      bottom: 0,
-      right: 0
-    };
-
-    if (elDim) {
-      // If it's not in flow (like 'float' element)
-      if (!elDim.inFlow) {
-        w = null;
-        h = elDim.outerHeight - marg * 2;
-        t = elDim.top + marg;
-        l =
-          where == "before" ? elDim.left - marg : elDim.left + elDim.outerWidth - marg;
-        margin = {
-          top: 0,
-          left: -brd,
-          bottom: 0,
-          right: 0
-        };
-      } else {
-        w = elDim.outerWidth;
-        h = null;
-        t =
-          where == "before" ? elDim.top - marg : elDim.top + elDim.outerHeight - marg;
-        l = elDim.left;
-      }
-    } else {
-      if (canvasDOMInfo) {
-        t = canvasDOMInfo.top + margI;
-        l = canvasDOMInfo.left + margI;
-        w = canvasDOMInfo.outerWidth - margI * 2;
-        h = null;
-      }
-    }
-
-    return {
-      top: t,
-      left: l,
-      width: w,
-      height: h,
-      margin
-    };
-  }
-
-  findPosition(
-    parent: CanvasNode,
-    dims: NodeInfo[],
-    posX: number,
-    posY: number
-  ) {
-    let result: DropAction = {
-      parent,
-      index: 0,
-      where: "before"
-    };
-
-    let leftLimit = 0,
-      xLimit = 0,
-      dimRight = 0,
-      yLimit = 0,
-      xCenter = 0,
-      yCenter = 0,
-      dimDown = 0,
-      dim = null,
-      id = null;
-    // Each dim is: Top, Left, Height, Width
-    for (var i = 0, len = dims.length; i < len; i++) {
-      dim = dims[i].dom;
-      id = dims[i].id as NodeId;
-
-      // Right position of the element. Left + Width
-      dimRight = dim.left + dim.outerWidth;
-      // Bottom position of the element. Top + Height
-      dimDown = dim.top + dim.outerHeight;
-      // X center position of the element. Left + (Width / 2)
-      xCenter = dim.left + dim.outerWidth / 2;
-      // Y center position of the element. Top + (Height / 2)
-      yCenter = dim.top + dim.outerHeight / 2;
-      // Skip if over the limits
-      if (
-        (xLimit && dim.left > xLimit) ||
-        (yLimit && yCenter >= yLimit) || // >= avoid issue with clearfixes
-        (leftLimit && dimRight < leftLimit)
-      )
-        continue;
-
-      result.index = i;
-      // If it's not in flow (like 'float' element)
-      if (!dim.inFlow) {
-        if (posY < dimDown) yLimit = dimDown;
-        //If x lefter than center
-        if (posX < xCenter) {
-          xLimit = xCenter;
-          result.where = "before";
-        } else {
-          leftLimit = xCenter;
-          result.where = "after";
-        }
-      } else {
-        // If y upper than center
-        if (posY < yCenter) {
-          result.where = "before";
-          break;
-        } else result.where = "after"; // After last element
-      }
-    }
-
-    return result;
-  }
 
   getNearestTarget(e: MouseEvent) {
     const { dragging, nodes }: BuilderContextState = this.context;
