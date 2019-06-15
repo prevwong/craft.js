51f49fbe526415b6e4e7f86b19fe14b8882be94f
Prev Wong <prevwong@gmail.com>
2019-06-16 02:46:06 +0800

    [feature/layers] 💄 beautify layers

--- packages/layers/RenderTreeNode.tsx
+++ packages/layers/RenderTreeNode.tsx
@@ -1,24 +1,38 @@
 import React from "react";
+import styled from "styled-components";
+import { List } from ".";
+import BuilderContext from "~packages/core/BuilderContext";
 
 export default class RenderTreeNode extends React.Component<any> {
     render() {
         const { node } = this.props;
         const {id, canvasName, type, props, children} = node;
         return (
-            <li>
-                <a>{typeof type === "function" ? type.name : type} {props.id ? props.id : null}</a>
-                {
-                    children && Object.keys(children).length && (
-                        <ul>
+            <BuilderContext.Consumer>
+                {({setNodeState, nodeState}) => {
+                    return (
+                        <li>
+                            <a onMouseDown={(e) => {
+                                e.stopPropagation();
+                                setNodeState("active", node.id)    
+                            }}>
+                                {typeof type === "function" ? type.name : type} {props.id ? props.id : null}
+                            </a>
                             {
-                                Object.keys(children).map((childId) => {
-                                    return <RenderTreeNode key={childId} node={children[childId]} />
-                                })
+                                children && Object.keys(children).length && (
+                                    <List child>
+                                        {
+                                            Object.keys(children).map((childId) => {
+                                                return <RenderTreeNode key={childId} node={children[childId]} />
+                                            })
+                                        }
+                                    </List>
+                                )
                             }
-                        </ul>
+                        </li>
                     )
-                }
-            </li>
+                }}
+            </BuilderContext.Consumer>
         )
     }
 }
