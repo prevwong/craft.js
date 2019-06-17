6cd021a7941a640432cf9394f67a167d59c4b82e
Prev Wong <prevwong@gmail.com>
2019-06-18 03:15:49 +0800

    [feature/refactor-render-process] :recycle: Make root node into a canvas

--- packages/core/render/Renderer.tsx
+++ packages/core/render/Renderer.tsx
@@ -4,31 +4,26 @@ import BuilderContext from "../BuilderContext";
 import { mapChildrenToNodes } from "../utils";
 import NodeElement from "../nodes/NodeElement";
 import RenderNodeWithContext from "./RenderNodeWithContext";
+import { Canvas } from "../nodes";
 
 export default class Renderer extends React.Component<any> {
  
   constructor(props: any, context: BuilderContextState) {
     super(props);
   
-    let node = mapChildrenToNodes(
-        <div id="root-node" style={{float:"left", width:"100%"}}>
-            {this.props.children}
-        </div>, null, "rootNode");
+    let node = mapChildrenToNodes(<Canvas>{this.props.children}</Canvas>, null, "rootNode");
 
     context.setNodes((nodes) => {
         nodes["rootNode"] = node["rootNode"];
-        console.log("set", nodes)
         return nodes;
     });
   }
 
 
   render() {
-    (window as any).tree = this.state;
     return (
       <BuilderContext.Consumer>
           {({nodes}) => {
-              console.log(nodes)
               return (
                 nodes["rootNode"]  && 
                     <NodeElement node={nodes["rootNode"]}>
