6a0cd8eab6a6fce26a6dda1e9f28ae3f6a12786a
Prev Wong <prevwong@gmail.com>
2019-06-20 00:22:02 +0800

    [feature/layers-package] :truck: move layer render to seperate component

--- packages/layers/RenderTreeNode.tsx
+++ packages/layers/RenderTreeNode.tsx
@@ -1,83 +1,86 @@
 import React, { SyntheticEvent } from "react";
+import ReactDOM from "react-dom";
 import styled from "styled-components";
 import { List } from ".";
 import LayerContext from "./context";
 import { getDOMInfo } from "./utils/dom";
 import { LayerContextState } from "./types";
+import LayerNodeContext from "./LayerNodeContext";
+import Comp from "./Comp";
+import { getDeepChildrenNodes } from "~packages/core/utils";
 
 export default class RenderTreeNode extends React.Component<any> {
   state = {
-    showChildren: false
+    showChildren: false,
+    collapse: false
   }
   ref = React.createRef();
 
   render() {
     const {showChildren} = this.state;
-    const { node } = this.props;
-    const { children } = node;
-    const layer = this.props.layer ? this.props.layer : 0;
+    const { layer } = this.props;
+    let { depth } = this.props;
+    const { children } = layer;
+
+    depth = depth ? depth : 0;
+
+    const el = <Comp />
 
     return (
       <LayerContext.Consumer>
-        {({ layerInfo, setDragging, placeholder, builder }: LayerContextState) => {
-          const { setNodeState } = builder;
+        {({ layersCollapsed, setLayerCollapse, layerInfo, setDragging, placeholder, builder }: LayerContextState) => {
+          const { setNodeState, active } = builder;
+          const drop = (placeholder && placeholder.nodeId === layer.id) ? placeholder.where : null;
+          const collapse = layersCollapsed[layer.id];
           return (
-            <LayerNode
-              ref={(dom) => {
-                if (dom) {
-                  // console.log("node", node.id);
-                  layerInfo[node.id].full = getDOMInfo(dom);
-                }
-              }}
-              
-            >
-              <LayerNodeMovementIndicator
-                style={{paddingLeft: `${(layer) * 10}px`}}
-                placeholderBefore={placeholder && placeholder.nodeId === node.id && placeholder.where === "before"}
-                placeholderAfter={placeholder && placeholder.nodeId === node.id && placeholder.where === "after"}
-                />
-              <LayerNodeTitle
-                style={{ paddingLeft: `${(layer) * 10}px` }}
-                placeholderInside={placeholder && placeholder.nodeId === node.id && placeholder.where === "inside"}
-                onMouseDown={(e) => {
-                  
-                  e.stopPropagation();
-                  e.nativeEvent.stopImmediatePropagation();
-                  setNodeState("active", node.id);
-                  if ( !node.parent ) return;
-                  setDragging(node.id);
-                  return false;
-                }}
+            <LayerNodeContext.Provider value={{
+              collapse,
+              layer,
+              setCollapse: (bool: Boolean) => {
+                setLayerCollapse(layer.id, bool);
+              }
+            }}>
+              <LayerNode
                 ref={(dom) => {
                   if (dom) {
-                    layerInfo[node.id] = getDOMInfo(dom);
+                    layerInfo[layer.id].full = getDOMInfo(dom);
                   }
                 }}
               >
-                <div className="nodename">{node.id}</div>
-                {
-                  (children && Object.keys(children).length) ? <a onMouseDown={(e) => {
-                    console.log("clicked")
-                    e.stopPropagation();
-                    this.setState({ showChildren: !showChildren })
-                  }}>Toggle</a> : null
-                }
-              </LayerNodeTitle>
-              {
-                (children && showChildren) ? (
-                  <List>
-                    {
-                      Object.keys(children).map((childId) => {
-                        return <RenderTreeNode key={childId} node={children[childId]} layer={layer + 1} />
-                      })
+                <div ref={
+                  (ref: any) => {
+                    if ( ref ) {
+                      layerInfo[layer.id] = getDOMInfo(ReactDOM.findDOMNode(ref) as HTMLElement);
                     }
-                  </List>
-                ) : null
-              }
-            </LayerNode>
+                }}>
+                  {
+                    React.cloneElement(el, {
+                      depth,
+                      layer,
+                      collapse,
+                      drop,
+                      builder,
+                      setLayerDragging: setDragging,
+                    })
+                  }
+                </div>
+                {
+                  (children && collapse) ? (
+                    <List>
+                      {
+                        Object.keys(children).map((childId) => {
+                          return <RenderTreeNode key={childId} layer={children[childId]} depth={depth + 1} />
+                        })
+                      }
+                    </List>
+                  ) : null
+                }
+              </LayerNode>
+            </LayerNodeContext.Provider>
           )
         }}
       </LayerContext.Consumer>
+      
     )
   }
 }
@@ -96,45 +99,5 @@ const LayerNode = styled.li`
   padding-bottom:1px;
 `
 
-const LayerNodeTitle = styled.div<{
-  placeholderInside: Boolean
-}>`
-  font-weight: lighter;
-  letter-spacing: 1px;
-  text-align: left;
-  position: relative;
-  cursor: pointer;
-  padding: 5px 0;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid rgba(0,0,0,0.25);
-  outline: ${props => props.placeholderInside ? "1px solid #000" : "none"};
-  > .nodename {
-    flex:1; 
-  }
-`
-
-const LayerNodeMovementIndicator = styled.div<{
-  placeholderBefore: Boolean
-  placeholderAfter: Boolean
-}>`
-  position:absolute;
-  height:100%;
-  width:100%;
-  &:after {
-    content: " ";
-    display:block; 
-    left:0;
-    width:100%;
-    height:1px;
-    display:block;
-    background:#000;
-    position:relative;
-    margin-top: -1px;
-    display: ${props => (props.placeholderBefore || props.placeholderAfter) ? "block" : "none" };
-    top: ${props => (props.placeholderBefore) ? 0 : "auto" };
-    bottom: ${props => (props.placeholderAfter) ? "-100%" : "auto" };
-  }
-`
 
 RenderTreeNode.contextType = LayerContext;
