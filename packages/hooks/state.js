e6f796b2707e7388c222b061eee748969281ec34
Prev Wong <prevwong@gmail.com>
2019-06-26 08:02:41 +0800

    [experiment/hooks] experimenting with hooks

---
+++ packages/hooks/state.js
@@ -0,0 +1,10 @@
+import React, {createContext, useContext, useReducer} from 'react';
+export const StateContext = createContext();
+
+export const StateProvider = ({reducer, initialState, children}) =>(
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
+
+export const useStateValue = () => useContext(StateContext);
