cb70d7e5724c4c71ea4255e044005efa5aee880c
Prev Wong <prevwong@gmail.com>
2019-06-29 03:15:37 +0800

    [experiment/hooks] :recycle: refactored events

---
+++ packages/core-refactor/events/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useContext } from "react";
+import { connectRoot } from "../manager";
+
+export const EventsManager = connectRoot(({children, manager: {state, methods}}) => {
+  const [placeholder, setPlaceholder] = useState(null);
+
+  useEffect(() => {
+    if (state.events.active) {
+      const onDrag = (e: React.MouseEvent) => {
+        const { left, right, top, bottom } = info[state.events.active]
+        if (
+          !(
+            e.clientX >= right &&
+            e.clientY >= top &&
+            e.clientY <= bottom
+          )
+        ) {
+          // Element being dragged
+          console.log("Dragging");
+        }
+      }
+      window.addEventListener("mousemove", onDrag);
+    }
+  }, [state.events.active]);
+
+  useEffect(() => {
+    window.addEventListener("mousedown", () => {
+      methods.setNodeEvent("active", null);
+    })
+  }, []);
+
+  return (
+    <React.Fragment>
+      {children}
+    </React.Fragment>
+  )
+})
