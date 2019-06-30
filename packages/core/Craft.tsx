1bc27baa076cb8d43e77ce36c75e5b5ea6071a9c
Prev Wong <prevwong@gmail.com>
2019-06-30 18:23:52 +0800

    [feature/migrate-hooks] use default export

--- packages/core/Craft.tsx
+++ packages/core/Craft.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ManagerContextProvider } from "./manager";
-import { EventsManager } from "./events";
+import EventsManager from "./events";
 
 export const Craft = ({children}: any) => {
   return (
