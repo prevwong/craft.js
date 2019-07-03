dc0cad98a5546d321283f64ee797241f978abe62
Prev Wong <prevwong@gmail.com>
2019-07-04 07:38:15 +0800

    [type-assertion] :recycle: refactored createNode

--- packages/core/shared/createNode.ts
+++ packages/core/shared/createNode.ts
@@ -1,34 +1,29 @@
 import produce from "immer";
-import { NodeId, Node, CanvasNode } from "../interfaces";
+import { NodeId, Node, NodeData } from "../interfaces";
 import React from "react";
 import { isCanvas } from "../nodes";
 
 // TODO: Refactor createNode()
-export function createNode(component: React.ElementType, props: React.Props<any>, id: NodeId, parent?: NodeId, closestParent?: NodeId, nodes?: NodeId[]): Node {
+export function createNode(data: Partial<NodeData> & Pick<NodeData, 'type' | 'props'>, id?: NodeId): Node {
   let node = produce({}, (node: Node) => {
     node.id = id;
     node.data = {
-      type: component as React.ElementType,
-      props: {...props},
-      parent: parent,
-      closestParent: parent ? parent : closestParent,
+      ...data,
       event: {
         active: false,
         dragging: false,
         hover: false
-      }
+      },
     };
 
     node.ref = {
       dom: null,
-      canDrag: () => true,
-      props: null
+      canDrag: () => true
     };
 
     if ( isCanvas(node) ) {
-      if ( nodes ) (node as CanvasNode).data.nodes = nodes;
-      (node as CanvasNode).ref.incoming = () => true;
-      (node as CanvasNode).ref.outgoing = () => true;
+      node.ref.incoming = () => true;
+      node.ref.outgoing = () => true;
     }
   }) as Node;
 
