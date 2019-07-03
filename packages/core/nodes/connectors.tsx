f7bbc87b235998cc23899fa0f47bf5d20114625b
Prev Wong <prevwong@gmail.com>
2019-07-03 18:40:20 +0800

    [feature/serializer] :sparkles: added serializer

--- packages/core/nodes/connectors.tsx
+++ packages/core/nodes/connectors.tsx
@@ -2,18 +2,18 @@ import React, { useMemo } from "react";
 import {useNode} from "../nodes";
 import { ConnectedNode } from "../interfaces";
 
-/**
- * Nearly identical to conenctInternalNode but hides internal manager methods and renderer
- * @param WrappedComponent 
- */
 export function connectNode<P extends ConnectedNode, C>(
   WrappedComponent: React.JSXElementConstructor<P>
 ) {  
   type Props = JSX.LibraryManagedAttributes<C, Omit<P, keyof ConnectedNode>>;
-  return (props: Props) => {
+  let ConnectedComponent = (props: Props) => {
     const target = useNode();
     return useMemo(() => <WrappedComponent {...target} {...props as any} />, [target.node]);
   }
+
+  (ConnectedComponent as any).displayName = WrappedComponent.name;
+
+  return ConnectedComponent;
 }
 
 
