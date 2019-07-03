115a9322367d2b56083bb5f946c7730748f86c6e
Prev Wong <prevwong@gmail.com>
2019-07-03 12:23:18 +0800

    [refactor/cleanup-exports] :fire: :recycle: cleanup exports

--- packages/core/index.tsx
+++ packages/core/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { ManagerContextProvider } from "./manager";
 import EventsManager from "./events";
-
+export * from "./events"
+export * from "./interfaces"
+export * from "./manager"
+export * from "./nodes"
+export * from "./render"
 export const Craft = ({children}: any) => {
   return (
     <ManagerContextProvider>
