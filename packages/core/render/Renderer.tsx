7da42fa3c9245fa393b4f99e29049f1d9ecd9b26
Prev Wong <prevwong@gmail.com>
2019-07-05 17:05:31 +0800

    [feature/placeholder] :truck: init events manager at renderer

--- packages/core/render/Renderer.tsx
+++ packages/core/render/Renderer.tsx
@@ -3,24 +3,27 @@ import { NodeElement, Canvas, mapChildrenToNodes } from "../nodes";
 import { useManager } from "../manager";
 import { RenderContext, RenderContextProvider } from "./RenderContext";
 import { Resolver } from "../interfaces";
+import { defaultPlaceholder } from "./RenderPlaceholder";
+import EventsManager from "../events";
 const invariant = require("invariant");
 
 export type Renderer = {
   nodes?: string
   resolver?: Resolver
-} & RenderContext
+} & Partial<RenderContext>
 
-export const Renderer: React.FC<Renderer> = ({ 
-  children, 
-  onRender = ({render}) => render,
+export const Renderer: React.FC<Renderer> = ({
+  children,
+  onRender = ({ render }) => render,
   resolver = {},
-  nodes = null 
+  renderPlaceholder = defaultPlaceholder,
+  nodes = null
 }) => {
-  const {rootNode, add, replaceNodes, query} = useManager((state) => ({rootNode: state.nodes["rootNode"]}));
+  const { rootNode, add, replaceNodes, query } = useManager((state) => ({ rootNode: state.nodes["rootNode"] }));
   useEffect(() => {
-    if ( !nodes ) { 
+    if (!nodes) {
       const rootCanvas = React.Children.only(children) as React.ReactElement;
-      invariant(rootCanvas.type  && rootCanvas.type == Canvas, "The immediate child of <Renderer /> has to be a Canvas");
+      invariant(rootCanvas.type && rootCanvas.type == Canvas, "The immediate child of <Renderer /> has to be a Canvas");
       let node = mapChildrenToNodes(rootCanvas, null, "rootNode");
       add(null, node);
     } else {
@@ -30,12 +33,14 @@ export const Renderer: React.FC<Renderer> = ({
   }, []);
 
   return useMemo(() => (
-    <RenderContextProvider onRender={onRender}>
-     {
-        rootNode ? (
-          <NodeElement id="rootNode" />
-        ) : null
-     }
+    <RenderContextProvider onRender={onRender} renderPlaceholder={renderPlaceholder}>
+      <EventsManager>
+        {
+          rootNode ? (
+            <NodeElement id="rootNode" />
+          ) : null
+        }
+      </EventsManager>
     </RenderContextProvider>
   ), [rootNode])
 }
