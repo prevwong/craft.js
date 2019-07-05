8eb1eec324de8d4f5e65c3dc9631e272b7023278
Prev Wong <prevwong@gmail.com>
2019-07-05 13:18:59 +0800

    [feature/serializer] :recycle: Use query method, updated typings

--- packages/core/render/Renderer.tsx
+++ packages/core/render/Renderer.tsx
@@ -1,45 +1,30 @@
-import React, { useEffect, useContext, useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
 import { NodeElement, Canvas, mapChildrenToNodes } from "../nodes";
-import { ManagerContext, useManager } from "../manager";
+import { useManager } from "../manager";
 import { RenderContext, RenderContextProvider } from "./RenderContext";
-import { ReactElement, NodeId } from "~types";
-import { NodeData, Nodes, SerializedNodeData } from "../interfaces";
-import { deserializeNode } from "../shared/deserializeNode";
-import { createNode } from "../shared/createNode";
+import { Resolver } from "../interfaces";
 const invariant = require("invariant");
 
 export type Renderer = {
-  nodes?: Record<NodeId, SerializedNodeData>
-  resolvers?: Function
+  nodes?: string
+  resolver?: Resolver
 } & RenderContext
 
 export const Renderer: React.FC<Renderer> = ({ 
   children, 
   onRender = ({render}) => render,
-  resolvers = () => {},
+  resolver = {},
   nodes = null 
 }) => {
-  const {rootNode, add, replaceNodes} = useManager((state) => ({rootNode: state.nodes["rootNode"]}));
+  const {rootNode, add, replaceNodes, query} = useManager((state) => ({rootNode: state.nodes["rootNode"]}));
   useEffect(() => {
     if ( !nodes ) { 
-      const rootCanvas = React.Children.only(children) as ReactElement;
+      const rootCanvas = React.Children.only(children) as React.ReactElement;
       invariant(rootCanvas.type  && rootCanvas.type == Canvas, "The immediate child of <Renderer /> has to be a Canvas");
       let node = mapChildrenToNodes(rootCanvas, null, "rootNode");
       add(null, node);
     } else {
-      const rehydratedNodes = Object.keys(nodes).reduce((accum, id) => {
-        const {type, props, parent, closestParent, nodes: childNodes} = deserializeNode(nodes[id], resolvers);
-        // TODO: Refactor createNode()
-        accum[id] = createNode({
-          type,
-          props,
-          parent, 
-          closestParent,
-          nodes: childNodes
-        }, id);
-
-        return accum;
-      }, {} as Nodes);
+      const rehydratedNodes = query.deserialize(nodes, resolver);
       replaceNodes(rehydratedNodes);
     }
   }, []);
