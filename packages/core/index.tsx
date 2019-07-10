87f2136284d8b4e4dea37c6a4db036b42d43212f
Prev Wong <prevwong@gmail.com>
2019-07-11 01:11:09 +0800

    [optimization] use monitor context

--- packages/core/index.tsx
+++ packages/core/index.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { createContext } from "react";
 import { ManagerContextProvider } from "./manager";
+import { MonitorProvider } from "./monitor/context";
 export * from "./events"
 export * from "./manager"
 export * from "./nodes"
 export * from "./render"
 export * from "./interfaces"
 
+
+const CraftContext = createContext(null);
+
+
 export const Craft = ({ children }: any) => {
   return (
-    <ManagerContextProvider>
-      {children}
-    </ManagerContextProvider>
+    <MonitorProvider>
+      <ManagerContextProvider>
+        {children}
+      </ManagerContextProvider>
+    </MonitorProvider>
   )
 }
