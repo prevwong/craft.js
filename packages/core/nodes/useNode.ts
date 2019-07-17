5c37f20ae4660e93ed406fab75525b1686f68e5b
Prev Wong <prevwong@gmail.com>
2019-07-17 13:10:56 +0800

    [feature/hover-event] handle native refs, onMouseOver and onMouseDown events

--- packages/core/nodes/useNode.ts
+++ packages/core/nodes/useNode.ts
@@ -1,6 +1,7 @@
 import React, { cloneElement, useMemo } from "react";
 import { useInternalNode } from "./useInternalNode";
 import { Node } from "../interfaces";
+import invariant from "invariant";
 
 
 export function useNode<S>(collect? : (node: Node) => S) {
@@ -10,20 +11,25 @@ export function useNode<S>(collect? : (node: Node) => S) {
     return {
       ...collected,
       actions: { setProp},
-      connectTarget: (render: React.ReactElement) => {
+      connectTarget: (render: any): React.ReactElement => {
+        const previousRef = render.ref;
+        invariant(previousRef !== "string", "Cannot connect to an element with an existing string ref. Please convert it into a callback ref instead.");
         return cloneElement(render, {
           ref: (ref: HTMLElement) => {
             if ( ref ) {
               setRef("dom", ref);
             }
+            if (previousRef) previousRef(ref);
           },
           onMouseOver: (e: React.MouseEvent) => {
             e.stopPropagation();
-            setNodeEvent('hover')
+            setNodeEvent('hover');
+            if (render.props.onMouseOver) render.props.onMouseOver(e);
           },
           onMouseDown: (e: React.MouseEvent) => {
             e.stopPropagation();
             setNodeEvent('active');
+            if ( render.props.onMouseDown ) render.props.onMouseDown(e);
           }
         })
       }
