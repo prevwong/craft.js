4c29b5715789eaf033bf31232b7b31b3a2e5d45c
Prev Wong <prevwong@gmail.com>
2019-07-17 14:48:25 +0800

    [feature/legacy-connectors] reintroduce legacy HOC

---
+++ packages/core/connectors/legacy/connectManager.tsx
@@ -0,0 +1,12 @@
+import { ManagerState } from "~packages/core/interfaces";
+import React from "react";
+import { useManager } from "../useManager";
+
+export function connectManager<C>(collect?: (state: ManagerState) => C) {
+  return (WrappedComponent: React.ElementType) => { 
+    return (props: any) => {
+      const manager = useManager(collect);
+      return <WrappedComponent {...manager} {...props} />;
+    }
+  } 
+}
