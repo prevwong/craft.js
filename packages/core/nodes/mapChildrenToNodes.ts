5de3a5e5e3e62a4964897c5292d94a2ba93cc02b
Prev Wong <prevwong@gmail.com>
2019-07-27 03:42:56 +0800

    [test] use transformJSX

--- packages/core/nodes/mapChildrenToNodes.ts
+++ packages/core/nodes/mapChildrenToNodes.ts
@@ -1,35 +1,18 @@
-import { NodeId, Node, NodeData } from "../interfaces";
-import React, { ReactNode, Fragment } from "react";
-import { Canvas } from "./Canvas";
-const shortid = require("shortid");
+import { NodeId, Node } from "../interfaces";
+import React, { ReactNode } from "react";
+import { transformJSXToNode } from "../utils/transformJSX";
 
 type MapChildrenData = {
   parent?: NodeId,
   hardId?: NodeId
 }
-export function mapChildrenToNodes(children: ReactNode, cb:(data: Partial<NodeData> & Pick<NodeData, 'type' | 'props'>, id: NodeId) => any, {hardId, parent}: MapChildrenData = {}): Node[] {
+export function mapChildrenToNodes(children: ReactNode, { hardId, parent }: MapChildrenData = {}, cb?: (data: Node) => any): Node[] {
   return React.Children.toArray(children).reduce(
     (result: Node[], child: React.ReactElement | string) => {
-      if (typeof (child) === "string") {
-        child = React.createElement(Fragment, {}, child);
-      }
-      let { type, props } = child;
-
-      // if (["string", "function"].includes(typeof (type))) {
-        const prefix = type === Canvas ? "canvas" : "node";
-        const id = hardId ? hardId : `${prefix}-${shortid.generate()}`;
-
-        let node = cb({
-          type: type,
-          props,
-          parent
-        }, id);
-        
-        result.push(node);
-        return result;
-      // }
-      // invariant(["string", "function"].includes(typeof (type)), "Invalid <Canvas> child provided. Expected simple JSX element or React Component.");
-
+      let node = transformJSXToNode(child, hardId, parent);
+      if ( cb ) node = cb(node);
+      result.push(node);
+      return result;
     },
     []
   ) as Node[]
