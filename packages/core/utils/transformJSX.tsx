d54749d56f14bb17267c99757ed781c158759115
Prev Wong <prevwong@gmail.com>
2019-07-27 05:21:11 +0800

    [test] cleanup transformToJsx

--- packages/core/utils/transformJSX.tsx
+++ packages/core/utils/transformJSX.tsx
@@ -4,14 +4,14 @@ import { NodeId, NodeRef, NodeData } from '../interfaces';
 import { createNode } from './createNode';
 const shortid = require('shortid');
 
-type Extras = { ref: Partial<NodeRef>, data: Partial<NodeData> };
-export function transformJSXToNode(child: React.ReactElement | string, hardId?: NodeId,  extras: Partial<Extras> = {ref: {}, data:{}}) {
+type Extras = { id: NodeId, ref: Partial<NodeRef>, data: Partial<NodeData> };
+export function transformJSXToNode(child: React.ReactElement | string, extras: Partial<Extras> = {id: null, ref: {}, data:{}}) {
   if (typeof (child) === "string") {
     child = React.createElement(Fragment, {}, child);
   }
   let { type, props } = child;
   const prefix = type === Canvas ? "canvas" : "node";
-  const id = hardId ? hardId : `${prefix}-${shortid.generate()}`;
+  const id = extras.id ? extras.id : `${prefix}-${shortid.generate()}`;
 
   return createNode({
     type: type,
