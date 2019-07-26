b8907c4fee4f145b7f9c27eeb173f3723d59388a
Prev Wong <prevwong@gmail.com>
2019-07-27 03:42:06 +0800

    [test] add constants

---
+++ packages/core/utils/constants.ts
@@ -0,0 +1,9 @@
+export const ROOT_NODE = 'ROOT'
+
+export const ERROR_INVALID_NODEID = 'Node does not exist, it may have been removed';
+export const ERROR_MISSING_PLACEHOLDER_PLACEMENT = 'Placeholder required placement info (parent, index, or where) is missing';
+export const ERROR_MOVE_INCOMING_PARENT = 'Target parent rejects incoming node';
+export const ERROR_MOVE_OUTGOING_PARENT = 'Current parent rejects outgoing node';
+export const ERROR_MOVE_NONCANVAS_CHILD = 'Cannot move node that is not a direct child of a Canvas node';
+export const ERROR_MOVE_TO_NONCANVAS_PARENT = 'Cannot move node into a non-Canvas parent';
+export const ERROR_MOVE_TO_DESCENDANT = 'Cannot move node into a descendant';
