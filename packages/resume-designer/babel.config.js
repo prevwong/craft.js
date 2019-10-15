ac8c5571afc195bdcaaea086f029511a47179187
Prev Wong <prevwong@gmail.com>
2019-10-16 04:09:35 +0800

    [master] cleanup

--- packages/resume-designer/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-    '@babel/preset-typescript'
-  ],
-  plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-runtime']
-}
--- packages/resume-designer/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {
-    'body': ['Fakt Pro'],
-  },
-  plugins: []
-}
