c109789ce03143ee6bd18001d16bc0b8a2bb0a2d
Prev Wong <prevwong@gmail.com>
2019-10-27 23:21:48 +0800

    [demo] ui improvements

--- packages/examples/resume-designer/src/index.tsx
+++ packages/examples/resume-designer/src/index.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom"
 import { Craft, Renderer, Canvas } from "craftjs";
 import { Container } from "./selectors/Container";
-import {Toolbar} from './components/Toolbar';
+import { Text } from "./selectors/Text";
+import { Editor } from './components/Editor';
+import { Toolbar } from './components/Toolbar';
+import { Toolbox } from './components/Toolbox';
 import "./demo.css";
 
 const App = () => {
@@ -14,25 +17,27 @@ const App = () => {
       <div className='fixed w-full h-full'>
         <div className="flex w-full h-full">
           <Craft
-            resolver={{ Container }}
+            resolver={{ Container, Text }}
           >
-            <div className="w-12 border-r bg-gray-100 h-full bg-white" />
+            <div className="w-12 border-r bg-gray-100 h-full bg-white">
+              <Toolbox />
+            </div>
             <div className="flex-1 h-full overflow-hidden">
               <div
-                className="bg-white h-full"
-                style={{ background: "#e2efff", width: "100%", height: "100%" }}
+                className="bg-white h-full w-full"
+                style={{ background: "#EEECF1", width: "100%", height: "100%" }}
               >
-                <Renderer>
-                  <Canvas is={Container} height="100%" width="100%" padding={[10, 10, 10, 10]}>
-                    <h2>Hi world</h2>
-                    <h3>what</h3>
+                <Renderer is={Editor}>
+                  <Canvas is={Container} background={{ r: 255, g: 255, b: 255, a: 1 }} height="100%" width="50%" paddingTop={2} paddingLeft={2} paddingBottom={2} paddingRight={2}>
+                    <Canvas is={Container} background={{ r: 0, g: 0, b: 0, a: 1 }} height="100%" width="50%" paddingTop={2} paddingLeft={2} paddingBottom={2} paddingRight={2}></Canvas>
+
                   </Canvas>
                 </Renderer>
               </div>
             </div>
             <div
-              style={{ background: "#2a52f5", width:"290px" }}
-                className="bg-gray-300 w-64 h-full"
+              style={{ background: "#2a52f5", width:"240px" }}
+                className="bg-gray-300 w-2 h-full"
               >
                   <Toolbar />
               </div>
