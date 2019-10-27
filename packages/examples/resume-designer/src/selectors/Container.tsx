fdcd24dfef5e0567f36428a330c7e7da0a11af4e
Prev Wong <prevwong@gmail.com>
2019-10-27 23:21:13 +0800

    [demo] update Container component

--- packages/examples/resume-designer/src/selectors/Container.tsx
+++ packages/examples/resume-designer/src/selectors/Container.tsx
@@ -1,15 +1,22 @@
 import React, { useRef, useLayoutEffect } from "react";
-import { Resizable } from "re-resizable";
-import { useNode, useManager } from "craftjs"
+import { Resizer, ResizerEditorSection, connectResize } from "../components/Resizer";
 import { EditorItem } from "../components/EditorItem"
 import { EditorSection } from "../components/EditorSection"
-import cx from "classnames";
+import { Input } from "../components/Input"
+import cx from 'classnames';
+
+import { useNode, useManager, Canvas } from "craftjs"
+import { Text } from "./Text";
 
 export type Container = {
-  bg: string;
+  background: string;
   flexDirection: "col" | "row";
-  width: number;
-  height: number;
+  width: string;
+  height: string;
+  paddingTop: number;
+  paddingLeft: number;
+  paddingBottom: number;
+  paddingRight: number;
   padding: number[];
   margin: number[];
   children: React.ReactNode;
@@ -19,170 +26,46 @@ export const Container = ({
   children,
   height,
   width,
-  bg = "#fff",
-  padding = [0, 0, 0, 0],
-  margin = [0, 0, 0, 0],
+  paddingTop = 0,
+  paddingLeft = 0,
+  paddingRight = 0,
+  paddingBottom = 0,
   flexDirection = "row",
+  background = 'rgba(0,0,0,0)'
 }: Partial<Container>) => {
-  const { actions, connectTarget, parent, dom } = useNode(node => ({
-    dom: node.ref.dom,
-    parent: node.data.parent,
-  }));
-  const startDrag = useRef<Record<"width" | "height", number>>({
-    width: 0,
-    height: 0,
-  });
-  const { parentDOM } = useManager(state => ({
-    parentDOM: parent && state.nodes[parent].ref.dom,
-  }));
 
-  useLayoutEffect(() => {
-    if (!dom && !parentDOM) return;
-    actions.setProp((prop: Container) => {
-      if (parentDOM) {
-        prop.width = parentDOM.getBoundingClientRect().width;
-      } else {
-        prop.width = dom.parentElement.getBoundingClientRect().width;
-      }
-
-      prop.height = dom.getBoundingClientRect().height;
-    });
-  }, [dom]);
-
-  return connectTarget(
-    <div style={{width, height}}>
-      <Resizable
-        className={cx(["flex", `flex-${flexDirection}`])}
-        size={{ width, height }}
-        onResizeStart={(e, d, refToElement) => {
-          const parent = refToElement.parentElement.getBoundingClientRect();
-          startDrag.current.width = width ? width : parent.width;
-          startDrag.current.height = height
-            ? height
-            : refToElement.getBoundingClientRect().height;
-        }}
-        onResize={(e, direction, ref, d) => {
-          const parent = ref.parentElement.getBoundingClientRect();
-          const rect = ref.getBoundingClientRect();
-
-          actions.setProp((prop: Container) => {
-            prop.width = startDrag.current.width + d.width;
-            prop.height = startDrag.current.height + d.height;
-          });
-        }}
-        style={{ background: bg, padding: padding.join("px ") + "px" }}
-
-      >
+  return (
+    <Resizer
+      propKey={{ width: "width", height: "height" }}
+      style={{ background: `rgba(${Object.values(background)})`, paddingLeft: `${paddingLeft}px`, paddingRight: `${paddingRight}px`, paddingBottom: `${paddingBottom}px`, paddingTop: `${paddingTop}px` }}
+    >
         {children}
-      </Resizable>
-    </div>
+    </Resizer>
   );
 };
 
 Container.related = {
   toolbar: () => {
-    const { actions, props: { width, height } } = useNode((state) => ({ props: state.data.props }));
     return (
       <React.Fragment>
-        <EditorSection title="Size">
-          <div className="flex">
-            <EditorItem prefix="W">
-              <input
-                onChange={e => {
-                  try {
-                    const num: number = parseInt(e.currentTarget.value);
-                    actions.setProp((prop: Container) => {
-                      prop.width = num;
-                    });
-                  } catch (err) { }
-                }}
-                className="editorInput"
-                type="text"
-                value={width || 0}
-              />
-            </EditorItem>
-            <EditorItem prefix="H">
-              <input
-                onChange={e => {
-                  try {
-                    const num: number = parseInt(e.currentTarget.value);
-                    actions.setProp((prop: Container) => {
-                      prop.height = num;
-                    });
-                  } catch (err) { }
-                }}
-                className="editorInput"
-                type="text"
-                value={height || 0}
-              />
-            </EditorItem>
-          </div>
-        </EditorSection>
+        <ResizerEditorSection />
         <EditorSection title="Padding">
-          <div className="flex">
-            <EditorItem prefix="T">
-              <input
-                onChange={e => {
-                  try {
-                    const num: number = parseInt(e.currentTarget.value);
-                    actions.setProp((prop: Container) => {
-                      prop.width = num;
-                    });
-                  } catch (err) { }
-                }}
-                className="editorInput"
-                type="text"
-                value={width || 0}
-              />
-            </EditorItem>
-            <EditorItem prefix="B">
-              <input
-                onChange={e => {
-                  try {
-                    const num: number = parseInt(e.currentTarget.value);
-                    actions.setProp((prop: Container) => {
-                      prop.height = num;
-                    });
-                  } catch (err) { }
-                }}
-                className="editorInput"
-                type="text"
-                value={height || 0}
-              />
-            </EditorItem>
-          </div>
-          <div className="flex mt-1">
-            <EditorItem prefix="L">
-              <input
-                onChange={e => {
-                  try {
-                    const num: number = parseInt(e.currentTarget.value);
-                    actions.setProp((prop: Container) => {
-                      prop.height = num;
-                    });
-                  } catch (err) { }
-                }}
-                className="editorInput"
-                type="text"
-                value={height || 0}
-              />
-            </EditorItem>
-            <EditorItem prefix="R">
-              <input
-                onChange={e => {
-                  try {
-                    const num: number = parseInt(e.currentTarget.value);
-                    actions.setProp((prop: Container) => {
-                      prop.height = num;
-                    });
-                  } catch (err) { }
-                }}
-                className="editorInput"
-                type="text"
-                value={height || 0}
-              />
-            </EditorItem>
-          </div>
+          <EditorItem prefix="T" propKey='paddingTop'
+              type="number"
+            />
+          <EditorItem prefix="L" propKey='paddingLeft'
+              type="number"
+            />
+
+          <EditorItem prefix="R"
+            propKey='paddingRight'
+            type="number"/>
+            
+          <EditorItem prefix="B" propKey='paddingBottom' type="number" />
+         
+        </EditorSection>
+        <EditorSection title="Background">
+          <EditorItem full={true} propKey='background' type="color" />
         </EditorSection>
       </React.Fragment>
     );
