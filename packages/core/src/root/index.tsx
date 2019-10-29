1e05a1be9abe1d91594950c1f7e7fce6685d3e17
Prev Wong <prevwong@gmail.com>
2019-10-30 02:30:36 +0800

    [fix/dnd] move dnd context to root

--- packages/core/src/root/index.tsx
+++ packages/core/src/root/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Options } from "../interfaces";
 import { RootContextProvider, createRootContext } from "./RootContext";
+import { DNDManager } from '../dnd/DNDManager';
 
 export const Craft: React.FC<Partial<Options>> = ({ children, ...options }: any) => {
   return (
     <RootContextProvider context={createRootContext({options})}>
-      {children}
+      <DNDManager>
+        {children}
+      </DNDManager>
     </RootContextProvider>
   )
 }
