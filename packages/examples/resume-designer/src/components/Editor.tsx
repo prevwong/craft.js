767edd32e3fd1ddfe30997b70189625cc44ea766
Prev Wong <prevwong@gmail.com>
2019-10-30 02:32:08 +0800

    [develop] update demo

--- packages/examples/resume-designer/src/components/Editor.tsx
+++ packages/examples/resume-designer/src/components/Editor.tsx
@@ -1,20 +1,23 @@
 import React, { useEffect } from 'react';
 import { useManager } from 'craftjs';
-import ResizeObserver from '@juggle/resize-observer';
 import { useState } from 'react';
-import { useMemo } from 'react';
 
 export const Editor:React.FC = ({children, ...props}) => {
-  const { activeDOM, activeProps} = useManager((state) => ({
+  const { activeDOM, activeProps, closestParent } = useManager((state) => ({
     activeDOM: state.events.active && state.events.active.ref.dom,
-    activeProps: state.events.active && state.events.active.data.props
+    activeProps: state.events.active && state.events.active.data.props,
+    closestParent: state.events.active && state.events.active.data.closestParent
   }));
-  const [observerStyle, setObserverStyle] = useState({});
-
- 
+  const [observerStyle, setObserverStyle] = useState({
+    width:0,
+    height:0,
+    left:0,
+    top:0
+  });
 
   useEffect(() => {
-    if (activeDOM && activeProps ) {
+    console.log("changed", closestParent)
+    if (activeDOM ) {
      setTimeout(() => {
         const { width, height, top, left } = activeDOM.getBoundingClientRect();
         setObserverStyle({
@@ -26,18 +29,16 @@ export const Editor:React.FC = ({children, ...props}) => {
      })
     }
 
-  }, [activeDOM, activeProps]);
+  }, [activeDOM, activeProps, closestParent ]);
 
   // if (active) console.log(getComputedStyle(active.ref.dom).marginLeft, active.ref.dom.getBoundingClientRect().left)
   return (
     <div style={{ borderColor: "#EEECF1" }} className="p-4 w-full h-full overflow-auto flex items-center"  {...props}>
-      {
-        activeProps && <div 
-          className='pointer-events-none fixed border-dashed border z-50 border-black'
-          style = {observerStyle}
-        />
-       
-      }
+        {
+          activeDOM && (
+          <div className='pointer-events-none fixed border-dashed border z-50 border-black' style={observerStyle}/>
+          )
+        }
         {children} 
     </div>
   )
