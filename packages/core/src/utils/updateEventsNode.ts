78ad3092f0f1a0a242eb38b9e21c6e666db81fc1
Prev Wong <prevwong@gmail.com>
2019-10-30 16:06:40 +0800

    [remove-closestParent] cleanup

--- packages/core/src/utils/updateEventsNode.ts
+++ packages/core/src/utils/updateEventsNode.ts
@@ -1,7 +1,7 @@
-import {Node, ManagerState, NodeId} from '../interfaces'
+import {Node, ManagerState, NodeId, NodeEvents} from '../interfaces'
 export const updateEventsNode = (state: ManagerState, id: NodeId) =>  {
   const node = state.nodes[id];
-  Object.keys(state.events).forEach(key => {
+  Object.keys(state.events).forEach((key: NodeEvents) => {
     if ( state.events[key] && state.events[key].id == node.id ) {
       state.events[key] = node;
     }
