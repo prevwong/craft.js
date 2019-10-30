44ee60e291d3a8f151ee5c5f163e6ce18c17c116
Prev Wong <prevwong@gmail.com>
2019-10-30 17:47:54 +0800

    [fix/prevent-infinite-canvas] use passThrough prop to determine if Canvas is rendered by a parent Canvas

--- packages/core/src/render/RenderNode.tsx
+++ packages/core/src/render/RenderNode.tsx
@@ -2,6 +2,7 @@ import React, { useMemo, useContext } from "react";
 import { useInternalNode } from "../nodes/useInternalNode";
 import { useNode } from "../connectors";
 import { RootContext } from "../root/RootContext";
+import { Canvas } from "../nodes";
 
 export const SimpleElement = ({render}: any) => {
   const {connectTarget} = useNode();
@@ -16,8 +17,10 @@ export const RenderNodeToElement: React.FC<any> =React.memo(({ ...injectedProps}
 
   return useMemo(() => {
     let Comp = type;
-    let render = React.cloneElement(<Comp {...props} {...injectedProps} />);
+    let render = React.cloneElement(<Comp {...props} {...injectedProps}  />);
     if (typeof Comp === 'string') render = <SimpleElement render={render} />
+    // To indicate the <Canvas /> is part of another Canvas' children
+    else if (Comp == Canvas ) render = React.cloneElement(render, {passThrough: true}); 
 
     return React.createElement(onRender, {render}, null);
   }, [type, props]);
