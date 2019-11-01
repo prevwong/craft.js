54583ab175e7afd91610691f2c5f8f75495232f5
Prev Wong <prevwong@gmail.com>
2019-11-02 02:58:31 +0800

    [refactor/manager] combine root and manager

--- packages/core/src/manager/useInternalManager.ts
+++ packages/core/src/manager/useInternalManager.ts
@@ -2,25 +2,15 @@ import React, { useContext} from "react";
 import { ManagerState, Options } from "../interfaces";
 import { QueryMethods } from "./query";
 import { useCollector } from "craftjs-utils";
-import { MethodRecordBase, ActionByType, ActionUnion } from "craftjs-utils";
-import { RootContext } from "../root/RootContext";
 import Actions from "./actions";
+import { ManagerContext } from "./ManagerContext";
 
 export type useInternalManager<C = null> = (C extends null ? useCollector<typeof Actions, typeof QueryMethods> : useCollector<typeof Actions, typeof QueryMethods, C>) & {_inContext: boolean};
 export function useInternalManager(): useInternalManager
 export function useInternalManager<C>(collector: (state: ManagerState) => C): useInternalManager<C>
 export function useInternalManager<C>(collector?: any): useInternalManager<C> {
-  const manager = useContext(RootContext);
+  const manager = useContext<ManagerContext>(ManagerContext);
   const collected = manager ? useCollector(manager, collector, (collected, finalize) => finalize(collected)) : {actions: {}};
-
-  // const queryFactory: QueryMethods = QueryMethods;
-  // const query = (Object.keys(queryFactory()) as Array<keyof QueryCallbacksFor<typeof queryFactory>>).reduce((accum: Partial<QueryCallbacksFor<typeof queryFactory>>, key) => {
-  //   return {
-  //     ...accum,
-  //     [key]: (...args: any) => queryFactory(manager.getState().current, options)[key](...args)
-  //   };
-  // }, {});
-
 
   return {
     ...collected as any,
