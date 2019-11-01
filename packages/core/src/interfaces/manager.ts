64fdad6c146df2f8cdc95d72d566e4d82ba27a2e
Prev Wong <prevwong@gmail.com>
2019-11-02 02:58:40 +0800

    [refactor/manager] update typings

--- packages/core/src/interfaces/manager.ts
+++ packages/core/src/interfaces/manager.ts
@@ -1,6 +1,16 @@
 import { Nodes, Node, NodeEvents } from "./nodes";
 import { PlaceholderInfo } from "../dnd/interfaces";
 import { useInternalManager } from "../manager/useInternalManager";
+import { Placeholder } from "../render/RenderPlaceholder";
+
+export type Options = {
+  onRender: React.FC<{ render: React.ReactElement }>,
+  renderPlaceholder: React.FC<Placeholder>
+  resolver: Resolver,
+  nodes: string
+}
+
+export type Resolver = Record<string, string | React.ElementType>;
 
 
 export type ManagerEvents = Record<NodeEvents, Node> & {
