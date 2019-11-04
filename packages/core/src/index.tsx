742b31d72170fd4bc0a2a1a92b0c09f21787273d
Prev Wong <prevwong@gmail.com>
2019-11-04 13:58:42 +0800

    [refactor/events] cleanup

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -4,15 +4,15 @@ export * from "./interfaces";
 export * from './connectors';
 import React from 'react';
 import { Options } from "interfaces";
-import { DNDManager } from "./dnd/DNDManager";
+import { EventManager } from "./dnd/EventManager";
 import {ManagerContextProvider, createManagerContext} from "./manager";
 
 export const Craft: React.FC<Partial<Options>> = ({ children, ...options }: any) => {
   return (
     <ManagerContextProvider context={createManagerContext({ options })}>
-      <DNDManager>
+      <EventManager>
         {children}
-      </DNDManager>
+      </EventManager>
     </ManagerContextProvider>
   )
 }
