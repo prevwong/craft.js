30089e40d82b7bcf3164002e55855438d2f1a2b0
Prev Wong <prevwong@gmail.com>
2019-11-30 18:17:31 +0800

    use new connectors

--- packages/core/src/nodes/Selector.tsx
+++ packages/core/src/nodes/Selector.tsx
@@ -1,34 +1,20 @@
 import React from 'react';
 import { useManager } from '../connectors';
-import { transformJSXToNode } from '../utils/transformJSX';
 import invariant from 'invariant';
-import { ROOT_NODE } from 'craftjs-utils';
-import { useContext } from 'react';
-import { Node } from '../interfaces';
-import { useInternalManager } from '../manager/useInternalManager';
-
 export type Selector = { 
   render: JSX.Element
 }
 
 export const Selector: React.FC<Selector> = ({render, children, ...props}) => {
-  const {handlers, query} = useInternalManager();
+  const {handlers, connectors, query} = useManager();
 
   const child = React.Children.only(children) as React.ReactElement | string;
   invariant(typeof child !== 'string', '<Selector> child cannot be a string. Please use an element instead');
   
   if ( typeof child !== 'string' ) {
     return React.cloneElement(child, {
-      draggable: true,
-      onDragStart: (e: React.MouseEvent) => {
-        e.stopPropagation();
-        const node = query.transformJSXToNode(React.createElement(render.type, render.props));
-        handlers.dnd.onDragStart(e, node as Node);
-      },
-      onDragEnd: (e: React.MouseEvent) => {
-        e.stopPropagation();
-        handlers.dnd.onDragEnd(e);
-      }
+      draggable: true,   
+      ref: (ref) => connectors.drag(ref, query.transformJSXToNode(React.createElement(render.type, render.props)))
     });
   }
   
