4b42c198d51b00ffc0ceb66c6b683ba5a9d177f0
Prev Wong <prevwong@gmail.com>
2019-12-21 18:30:48 +0800

    fix scripts

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -2,8 +2,8 @@
 lerna run export --stream
 rm -rf site/
 mkdir site
+touch site/.nojekyll
 cp -r ./packages/examples/content-editor/out/* site/
-touch .nojekyll ./packages/site
 cp -r ./packages/docs/build site/r
 mkdir site/examples
 cp -r ./packages/examples/nextjs/out/ site/examples/basic
