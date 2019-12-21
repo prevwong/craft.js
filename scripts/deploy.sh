77df52d4b0915860afbab74cc687b3b7d5487eb4
Prev Wong <prevwong@gmail.com>
2019-12-21 15:25:14 +0800

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@ STATUS="$(git status)"
 
 if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
 then
-    sed -i "" '/site' ./.gitignore
+    sed -i "" '/site/d' ./.gitignore
     git add .
     git commit -m "Edit .gitignore to publish"
     git push origin `git subtree split --prefix site master`:gh-pages --force
