edbcb774e91d53f55775542515968b3d5b52ab5d
Prev Wong <prevwong@gmail.com>
2019-12-21 15:26:33 +0800

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ then
     sed -i "" '/site/d' ./.gitignore
     git add .
     git commit -m "Edit .gitignore to publish"
-    git push origin `git subtree split --prefix site master`:gh-pages --force
+    git subtree push --prefix site origin gh-pages
     git reset HEAD~
     git checkout .gitignore
 else
