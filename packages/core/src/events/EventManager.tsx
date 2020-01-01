d00e1b737b40186a5de02ef18a0b0a2e84b6de14
Prev Wong <prevwong@gmail.com>
2020-01-02 03:11:58 +0800

    fix docusaurus path + dnd ghost image

--- packages/core/src/events/EventManager.tsx
+++ packages/core/src/events/EventManager.tsx
@@ -14,6 +14,8 @@ const createShadow = (e: DragEvent) => {
     shadow.style.width = `${width}px`;
     shadow.style.height = `${height}px`;
     shadow.style.position = 'fixed';
+    shadow.style.left = '-100%';
+    shadow.style.top = '-100%';
     
     document.body.appendChild(shadow);
     e.dataTransfer.setDragImage(shadow, 0, 0);
