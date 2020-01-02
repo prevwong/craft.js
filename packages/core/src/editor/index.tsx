2c551c7c801332fd517cd956bd9f37341f123a78
Prev Wong <prevwong@gmail.com>
2020-01-02 13:13:11 +0800

    simplify

--- packages/core/src/editor/index.tsx
+++ packages/core/src/editor/index.tsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, {  useEffect } from "react";
 import { Options } from "../interfaces";
-import { EditorContextProvider } from "./EditorContextProvider";
+import {  useEditorStore } from "../editor/store";
+import { EditorContext } from "./EditorContext";
 import { EventManager } from "../events/EventManager";
 
+export const createEditorStoreOptions = (options: Partial<Options> = {}) => {
+  return {
+    onRender: ({ render }) => render,
+    resolver: {},
+    nodes: null,
+    enabled: true,
+    indicator: {
+      error: "red",
+      success: "rgb(98, 196, 98)"
+    },
+    ...options
+  };
+}
 
-export const Editor: React.FC<Partial<Options>> = ({ children, ...options }: any) => {
-  return (
-    <EditorContextProvider options={options}>
+
+export const Editor: React.FC<Partial<Options>> = ({ children, ...options }) => {
+ 
+  const context = useEditorStore(createEditorStoreOptions(options));
+
+  useEffect(() => {
+    if ( context && options ) 
+      context.actions.setOptions((editorOptions) => {
+        editorOptions = options;
+      });
+  }, [options]);
+  
+  return context ? (
+    <EditorContext.Provider value={context}>
       <EventManager>
-        {children}
+
+          {children}
       </EventManager>
-    </EditorContextProvider>
-  )
+    </EditorContext.Provider>
+  ) : null
 }
