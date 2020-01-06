51566421c9e2bdd6d043cd52fc7f7732e3f8629a
Prev Wong <prevwong@gmail.com>
2020-01-06 12:03:49 +0800

    fix demo font

--- packages/examples/landing/components/editor/Toolbar/index.tsx
+++ packages/examples/landing/components/editor/Toolbar/index.tsx
@@ -15,8 +15,13 @@ export const Toolbar = () => {
     <div className='py-1 h-full'>
       {active && related.toolbar && React.createElement(related.toolbar)}
       {!active &&
-        <div className="px-2 py-2 flex items-center h-full justify-center text-center" style={{color:"rgba(0, 0, 0, 0.5607843137254902)", fontSize: "11px"}}>
-          <h2>Click on a component to start editing</h2>
+        <div className="px-5 py-2 flex flex-col items-center h-full justify-center text-center" style={{color:"rgba(0, 0, 0, 0.5607843137254902)", fontSize: "11px"}}>
+          <h2 className="pb-1">
+            Click on a component to start editing.
+          </h2>
+          <h2>
+            You could also double click on the layers below to edit their names, like in Photoshop
+          </h2>
         </div>
       }
     </div>
