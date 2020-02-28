bd02af84bff19396fbd5b4ea2119f127fe2000fb
Prev Wong <prevwong@gmail.com>
2020-02-29 03:34:15 +0800

    Revert redirect url
    docusaurus probably runs its own shell. Hence, cross-env variables are not being passed to select which URL from the config file to be used. For now, we will hardcode the redirect url as the production url (craft.js.org)

--- packages/docs/src/pages/index.js
+++ packages/docs/src/pages/index.js
@@ -7,11 +7,10 @@
 
 import React from "react";
 import {Redirect} from '@docusaurus/router';
-import config from "../../../../config";
 
 function Home() {
   if (typeof window !== 'undefined' && process.env.NODE_ENV != "development") {
-    window.location.href=config.url["LANDING"];
+    window.location.href="https://craft.js.org/";
     return <p style={{padding: "10px", textAlign:"center"}}>Redirecting...</p>
   } else {
     return  <Redirect to="docs/overview" />;
