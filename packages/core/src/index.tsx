894b4b0d3e76045237526d8e25c004a8cea1aec0
Michele Riccardo Esposito <mresposito5@gmail.com>
2020-04-03 14:27:55 +0100

    expose use node context (#9)

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -4,3 +4,6 @@ export * from "./hooks";
 export * from "./interfaces";
 export * from "./nodes";
 export * from "./render";
+
+// We expose this methods for candu's internal usage. Use at your own risksi
+export { useInternalEditor } from "./editor/useInternalEditor";
