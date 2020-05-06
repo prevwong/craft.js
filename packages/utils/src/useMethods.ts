3ac6f8b0b53a866c94d3d4068a2b7f6ead434a02
Prev Wong <prevwong@gmail.com>
2020-05-06 10:24:59 +0800

    nit: remove comments

--- packages/utils/src/useMethods.ts
+++ packages/utils/src/useMethods.ts
@@ -212,12 +212,9 @@ class Watcher<S> {
   }
 
   notify() {
-    // Give unsubscribing the priority. Any better way?
-    // setTimeout(() => {
     for (let i = 0; i < this.subscribers.length; i++) {
       this.subscribers[i].collect();
     }
-    // });
   }
 }
 
