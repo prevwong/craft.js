9e4d3bff07f76fe7a518333212e4feeb954eae27
Prev Wong <prevwong@gmail.com>
2020-05-20 23:47:09 +0800

    feat: move connectors to context

--- packages/core/src/nodes/NodeElement.tsx
+++ packages/core/src/nodes/NodeElement.tsx
@@ -1,24 +1,15 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { NodeProvider } from "./NodeContext";
 import { RenderNodeToElement } from "../render/RenderNode";
 import { NodeId } from "../interfaces";
-import { NodeHandlers } from "./NodeHandlers";
-import { useEventHandler } from "../events";
 
 export type NodeElement = {
   id: NodeId;
 };
 
 export const NodeElement: React.FC<NodeElement> = React.memo(({ id }) => {
-  const handlers = useEventHandler();
-
-  const connectors = useMemo(
-    () => handlers.derive(NodeHandlers, id).connectors(),
-    [handlers, id]
-  );
-
   return (
-    <NodeProvider id={id} connectors={connectors}>
+    <NodeProvider id={id}>
       <RenderNodeToElement />
     </NodeProvider>
   );
