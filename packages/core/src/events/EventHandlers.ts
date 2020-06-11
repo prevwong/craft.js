e468c1fee088a7e2c6a51f812834df7bb8cc44a6
Prev Wong <prevwong@gmail.com>
2020-06-11 22:11:38 +0800

    fix: ensure events exist

--- packages/core/src/events/EventHandlers.ts
+++ packages/core/src/events/EventHandlers.ts
@@ -157,7 +157,12 @@ export class EventHandlers extends Handlers<
     ) => void
   ) {
     const { draggedElement, draggedElementShadow, events } = EventHandlers;
-    if (draggedElement && events.indicator && !events.indicator.error) {
+    if (
+      draggedElement &&
+      events &&
+      events.indicator &&
+      !events.indicator.error
+    ) {
       const { placement } = events.indicator;
       onDropNode(draggedElement, placement);
     }
