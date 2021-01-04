c0d8e91f92d05f3c08945391d5af99bc6bbad2ed
Prev Wong <prevwong@gmail.com>
2021-01-04 11:12:59 +0800

    chore: rollup

--- packages/slate/rollup.config.js
+++ packages/slate/rollup.config.js
@@ -1,9 +1,6 @@
-import image from '@svgr/rollup';
-
 import config from '../../rollup.config';
 
 export default {
   ...config,
   input: './src/index.ts',
-  plugins: [...config.plugins, image()],
 };
