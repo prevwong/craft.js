c47cacd9b40ebecd88133ff0f72407be0104a353
Prev Wong <prevwong@gmail.com>
2021-01-05 13:50:17 +0800

    chore: export testHelpers

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -4,4 +4,5 @@ export * from './interfaces';
 export * from './hooks';
 export * from './editor';
 export * from './events';
+export * from './utils/testHelpers';
 export { ROOT_NODE } from '@craftjs/utils';
