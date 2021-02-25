2988ca0e572a0f3ffafd780ffd0913a7e2bff2dc
Alex <ritt.alex@gmail.com>
2021-02-25 23:30:21 +0100

    Fix warnings

--- packages/examples/landing/pages/_document.tsx
+++ packages/examples/landing/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 // Import styled components ServerStyleSheet
 import { ServerStyleSheet } from 'styled-components';
 
@@ -21,7 +21,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <Html>
         <Head>
           {/* Step 5: Output the styles in the head  */}
           {(this.props as any).styleTags}
@@ -30,7 +30,7 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
