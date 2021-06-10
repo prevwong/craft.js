2751a67bd0169fff180221776df1857a8c8e62b2
Alex Koehler <alex@layer.team>
2021-06-10 16:37:54 -0500

    Replaced lodash with lodash-es in web bundles:
    - Updated all imports to use lodash-es instead of lodash
    - Use jest file replacement for tests
    - Added lodash dev dependency to support tests

--- packages/core/src/events/DefaultEventHandlers.ts
+++ packages/core/src/events/DefaultEventHandlers.ts
@@ -1,4 +1,4 @@
-import { isFunction } from 'lodash';
+import { isFunction } from 'lodash-es';
 
 import { CoreEventHandlers, CreateHandlerOptions } from './CoreEventHandlers';
 import { createShadow } from './createShadow';
