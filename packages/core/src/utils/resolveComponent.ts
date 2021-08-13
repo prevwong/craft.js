405c13d2fe226dd585b7b088e13eec3b5097b1dd
Prev Wong <prevwong@gmail.com>
2021-08-13 21:08:46 +0800

    feat: throw error if component is missing from resolver (#293)
    * feat: throw error if component is missing from resolver
    
    * chore: feedback
    
    * debug

--- packages/core/src/utils/resolveComponent.ts
+++ packages/core/src/utils/resolveComponent.ts
@@ -1,25 +1,39 @@
+import { ERROR_NOT_IN_RESOLVER } from '@craftjs/utils';
+import invariant from 'tiny-invariant';
+
 import { Resolver } from '../interfaces';
-import { Canvas } from '../nodes/Canvas';
 
 export const resolveComponent = (
   resolver: Resolver,
   comp: React.ElementType | string
 ) => {
-  let Comp: string;
-  const name = (comp as any).name || (comp as any).displayName;
+  const componentName = (comp as any).name || (comp as any).displayName;
 
-  if (comp === Canvas) return 'Canvas';
-
-  if (resolver[name]) return name;
-
-  for (let i = 0; i < Object.keys(resolver).length; i++) {
-    const name = Object.keys(resolver)[i],
-      fn = resolver[name];
-    if (fn === comp) {
-      Comp = name;
-      return Comp;
+  const getNameInResolver = () => {
+    if (resolver[componentName]) {
+      return componentName;
     }
-  }
 
-  if (typeof comp === 'string') return comp;
+    for (let i = 0; i < Object.keys(resolver).length; i++) {
+      const name = Object.keys(resolver)[i];
+      const fn = resolver[name];
+
+      if (fn === comp) {
+        return name;
+      }
+    }
+
+    if (typeof comp === 'string') {
+      return comp;
+    }
+  };
+
+  const resolvedName = getNameInResolver();
+
+  invariant(
+    resolvedName,
+    ERROR_NOT_IN_RESOLVER.replace('%node_type%', componentName)
+  );
+
+  return resolvedName;
 };
