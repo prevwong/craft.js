96a65bad47f4ec11ecef4383b73f5184242305e7
Josh Balfour <josh@joshbalfour.co.uk>
2022-02-28 15:03:45 +0000

    expose serializeNode

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -5,4 +5,5 @@ export * from './hooks';
 export * from './editor';
 export * from './events';
 export * from './utils/testHelpers';
+export { serializeNode } from './utils/serializeNode';
 export { ROOT_NODE } from '@craftjs/utils';
