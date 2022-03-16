d8ad5cfe1d11aefcfa2a9d3344a4061d86d3809e
Prev Wong <prevwong@gmail.com>
2022-03-16 23:12:43 +0800

    feat: add carbonads snippet

--- examples/landing/components/editor/Viewport/Sidebar/index.tsx
+++ examples/landing/components/editor/Viewport/Sidebar/index.tsx
@@ -16,6 +16,86 @@ export const SidebarDiv = styled.div<{ enabled: boolean }>`
   margin-right: ${(props) => (props.enabled ? 0 : -280)}px;
 `;
 
+const CarbonAdsContainer = styled.div`
+  width: 100%;
+  margin-top: auto;
+
+  #carbonads * {
+    margin: initial;
+    padding: initial;
+  }
+
+  #carbonads {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+      Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', Helvetica, Arial,
+      sans-serif;
+
+    padding: 10px 0.5rem;
+    border-top: 1px solid rgb(229 231 235);
+  }
+
+  #carbonads {
+    display: flex;
+    width: 100%;
+    background-color: transparent;
+    z-index: 100;
+  }
+
+  #carbonads a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  #carbonads a:hover {
+    color: inherit;
+  }
+
+  #carbonads span {
+    position: relative;
+    display: block;
+    overflow: hidden;
+  }
+
+  #carbonads .carbon-wrap {
+    display: flex;
+  }
+
+  #carbonads .carbon-img {
+    display: block;
+    margin: 0;
+    line-height: 1;
+  }
+
+  #carbonads .carbon-img img {
+    display: block;
+  }
+
+  #carbonads .carbon-text {
+    font-size: 11px;
+    padding: 10px;
+    margin-bottom: 16px;
+    line-height: 1.5;
+    text-align: left;
+    color: #333333;
+    font-weight: 400;
+  }
+
+  #carbonads .carbon-poweredby {
+    display: block;
+    padding: 6px 8px;
+    text-align: center;
+    text-transform: uppercase;
+    letter-spacing: 0.5px;
+    font-weight: 600;
+    font-size: 8px;
+    line-height: 1;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    color: #8f8f8f;
+  }
+`;
+
 export const Sidebar = () => {
   const [layersVisible, setLayerVisible] = useState(true);
   const [toolbarVisible, setToolbarVisible] = useState(true);
@@ -46,6 +126,14 @@ export const Sidebar = () => {
             <Layers expandRootOnLoad={true} />
           </div>
         </SidebarItem>
+        <CarbonAdsContainer>
+          <script
+            async
+            type="text/javascript"
+            src="//cdn.carbonads.com/carbon.js?serve=CEAI453N&placement=craftjsorg"
+            id="_carbonads_js"
+          ></script>
+        </CarbonAdsContainer>
       </div>
     </SidebarDiv>
   );
