ba76ba7e42a1820a82d17857f876f617611589cb
Prev Wong <prevwong@gmail.com>
2022-03-28 20:36:21 +0800

    feat: add history event listener (#399)

--- packages/utils/src/History.ts
+++ packages/utils/src/History.ts
@@ -6,14 +6,14 @@ type Timeline = Array<{
   timestamp: number;
 }>;
 
-export const HISTORY_ACTIONS = {
-  UNDO: 'HISTORY_UNDO',
-  REDO: 'HISTORY_REDO',
-  THROTTLE: 'HISTORY_THROTTLE',
-  IGNORE: 'HISTORY_IGNORE',
-  MERGE: 'HISTORY_MERGE',
-  CLEAR: 'HISTORY_CLEAR',
-};
+export enum HISTORY_OPERATION {
+  UNDO = 'HISTORY_UNDO',
+  REDO = 'HISTORY_REDO',
+  CLEAR = 'HISTORY_CLEAR',
+  ADD = 'HISTORY_ADD',
+}
+
+export type HistoryListener = (op: HISTORY_OPERATION) => void;
 
 export type HistoryMergeOpts = {
   ignoreIfNoPreviousRecords: boolean;
@@ -22,6 +22,8 @@ export type HistoryMergeOpts = {
 export class History {
   timeline: Timeline = [];
   pointer = -1;
+
+  listeners: Set<HistoryListener> = new Set();
 
   add(patches: Patch[], inversePatches: Patch[]) {
     if (patches.length === 0 && inversePatches.length === 0) {
@@ -35,6 +37,8 @@ export class History {
       inversePatches,
       timestamp: Date.now(),
     };
+
+    this.notify(HISTORY_OPERATION.ADD);
   }
 
   throttleAdd(
@@ -105,6 +109,8 @@ export class History {
   clear() {
     this.timeline = [];
     this.pointer = -1;
+
+    this.notify(HISTORY_OPERATION.CLEAR);
   }
 
   canUndo() {
@@ -122,6 +128,9 @@ export class History {
 
     const { inversePatches } = this.timeline[this.pointer];
     this.pointer = this.pointer - 1;
+
+    this.notify(HISTORY_OPERATION.UNDO);
+
     return applyPatches(state, inversePatches);
   }
 
@@ -132,6 +141,19 @@ export class History {
 
     this.pointer = this.pointer + 1;
     const { patches } = this.timeline[this.pointer];
+
+    this.notify(HISTORY_OPERATION.REDO);
+
     return applyPatches(state, patches);
+  }
+
+  listen(listener: HistoryListener) {
+    this.listeners.add(listener);
+
+    return () => this.listeners.delete(listener);
+  }
+
+  private notify(op: HISTORY_OPERATION) {
+    this.listeners.forEach((listener) => listener(op));
   }
 }
