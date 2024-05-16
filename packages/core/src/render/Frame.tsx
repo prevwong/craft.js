e4c6692e296563d44e6258d2f0592af9f4381a15
Tomas Cerskus <tomas-c@users.noreply.github.com>
2024-05-16 18:06:37 +0100

    Completely get rid of useEffect in Frame component

--- packages/core/src/render/Frame.tsx
+++ packages/core/src/render/Frame.tsx
@@ -1,5 +1,5 @@
 import { deprecationWarning, ROOT_NODE } from '@craftjs/utils';
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 
 import { useInternalEditor } from '../editor/useInternalEditor';
 import { SerializedNodes } from '../interfaces';
@@ -39,41 +39,28 @@ export const Frame: React.FC<React.PropsWithChildren<FrameProps>> = ({
     });
   }
 
-  const initialState = useRef({
-    initialChildren: children,
-    initialData: data || json,
-  });
+  const isLoaded = useRef(false);
 
-  const isInitialLoadedRef = useRef(false);
+  if (!isLoaded.current) {
+    const initialData = data || json;
 
-  if (!isInitialLoadedRef.current && initialState.current.initialData) {
-    actions.history.ignore().deserialize(initialState.current.initialData);
-    isInitialLoadedRef.current = true;
-  }
+    if (initialData) {
+      actions.history.ignore().deserialize(initialData);
+    } else {
+      const rootNode = React.Children.only(children) as React.ReactElement;
 
-  useEffect(() => {
-    const { initialChildren } = initialState.current;
+      const node = query.parseReactElement(rootNode).toNodeTree((node, jsx) => {
+        if (jsx === rootNode) {
+          node.id = ROOT_NODE;
+        }
+        return node;
+      });
 
-    // Prevent recreating Nodes from child elements if we already did it the first time
-    // Usually an issue in React Strict Mode where this hook is called twice which results in orphaned Nodes
-    const isInitialLoaded = isInitialLoadedRef.current;
-
-    if (!initialChildren || isInitialLoaded) {
-      return;
+      actions.history.ignore().addNodeTree(node);
     }
 
-    const rootNode = React.Children.only(initialChildren) as React.ReactElement;
-
-    const node = query.parseReactElement(rootNode).toNodeTree((node, jsx) => {
-      if (jsx === rootNode) {
-        node.id = ROOT_NODE;
-      }
-      return node;
-    });
-
-    actions.history.ignore().addNodeTree(node);
-    isInitialLoadedRef.current = true;
-  }, [actions, query]);
+    isLoaded.current = true;
+  }
 
   return <RenderRootNode />;
 };
