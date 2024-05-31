cf1d4c06e237961eec56d78f6a71379523336c04
Prev Wong <prevwong@gmail.com>
2024-05-31 11:17:33 +0800

    fix: ssr hydration error in strict mode

--- packages/core/src/nodes/Element.tsx
+++ packages/core/src/nodes/Element.tsx
@@ -40,42 +40,33 @@ export function Element<T extends React.ElementType>({
   };
 
   const { query, actions } = useInternalEditor();
-  const { node, inNodeContext } = useInternalNode((node) => ({
-    node: {
-      id: node.id,
-      data: node.data,
-    },
-  }));
+  const { id: nodeId, inNodeContext } = useInternalNode();
 
   const [linkedNodeId] = useState<NodeId | null>(() => {
     invariant(!!id, ERROR_TOP_LEVEL_ELEMENT_NO_ID);
-    const { id: nodeId, data } = node;
+    const node = query.node(nodeId).get();
 
     if (inNodeContext) {
-      let linkedNodeId;
-
-      const existingNode =
-        data.linkedNodes &&
-        data.linkedNodes[id] &&
-        query.node(data.linkedNodes[id]).get();
+      const existingNode = node.data.linkedNodes[id]
+        ? query.node(node.data.linkedNodes[id]).get()
+        : null;
 
       // Render existing linked Node if it already exists (and is the same type as the JSX)
       if (existingNode && existingNode.data.type === is) {
-        linkedNodeId = existingNode.id;
-      } else {
-        // otherwise, create and render a new linked Node
-        const linkedElement = React.createElement(
-          Element,
-          elementProps,
-          children
-        );
-
-        const tree = query.parseReactElement(linkedElement).toNodeTree();
-
-        linkedNodeId = tree.rootNodeId;
-        actions.history.ignore().addLinkedNodeFromTree(tree, nodeId, id);
+        return existingNode.id;
       }
-      return linkedNodeId;
+
+      // otherwise, create and render a new linked Node
+      const linkedElement = React.createElement(
+        Element,
+        elementProps,
+        children
+      );
+
+      const tree = query.parseReactElement(linkedElement).toNodeTree();
+
+      actions.history.ignore().addLinkedNodeFromTree(tree, nodeId, id);
+      return tree.rootNodeId;
     }
     return null;
   });
