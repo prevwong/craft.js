cba601be92c8661d3d12fe517fd6c0365496d081
Mateusz <mateusz@rogosz.pl>
2024-06-03 13:02:00 +0200

    fix: add export for createShadow

--- packages/core/src/events/index.tsx
+++ packages/core/src/events/index.tsx
@@ -3,3 +3,4 @@ export * from './CoreEventHandlers';
 export * from './DefaultEventHandlers';
 export { Events } from './Events';
 export { Positioner } from './Positioner';
+export { createShadow } from './createShadow';
